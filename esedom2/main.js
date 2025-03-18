00d12b377cbcb4942c64f71838c2ad2f45a0cafe
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-03-18 12:17:39 +0100

    Update main.js

--- esedom2/main.js
+++ esedom2/main.js
@@ -5,14 +5,15 @@ let container = document.querySelector('.row');
 
 button.addEventListener('click' , ()=>{
 
-     
+    let date = new Date();
+    let formatDate = date.toLocaleDateString()
 
     if(input.value == `` && text.value == ``){
         alert('metti qualcosa o non si crea nulla');
 
     }else{
         let div = document.createElement('div');
-        div.innerHTML = `<h2>${input.value}</h2> <p>${text.value}</p>`;
+        div.innerHTML = `<h2>${input.value}</h2> <p>${text.value}</p> <i>${formatDate}</i>`;
         
         container.appendChild(div);
 
